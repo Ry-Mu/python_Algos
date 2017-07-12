147fa9888f6959cee4432331e98c51ae1f8127da
Ryan Munguia <ryan.munguia92@gmail.com>
2017-07-11 19:10:33 -0700

    adding oddTuple file

---
+++ algosLevel1/oddTuples.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 11 18:58:00 2017
+
+@author: RyanMunguia
+"""
+
+#Write a procedure called oddTuples, which takes a tuple as input, and returns a
+#new tuple as output, where every other element of the input tuple is copied, 
+#starting with the first one. So if test is the tuple ('I', 'am', 'a', 'test', 'tuple'), 
+#then evaluating oddTuples on this input would return the tuple ('I', 'a', 'tuple').
+
+def oddTuples(aTup):
+    '''
+    aTup: a tuple
+    
+    returns: tuple, every other element of aTup. 
+    '''
+    # a placeholder to gather our response
+    rTup = ()
+    index = 0
+
+    # Idea: Iterate over the elements in aTup, counting by 2
+    #  (every other element) and adding that element to 
+    #  the result
+    while index < len(aTup):
+        rTup += (aTup[index],)
+        index += 2
+
+    return rTup
+
+def oddTuples2(aTup):
+    '''
+    Another way to solve the problem.
+ 
+    aTup: a tuple
+     
+    returns: tuple, every other element of aTup. 
+    '''
+    # Here is another solution to the problem that uses tuple 
+    #  slicing by 2 to achieve the same result
+    return aTup[::2]
+
+print(oddTuples(('I', 'am', 'a', 'test', 'tuple')))
+print(oddTuples2(('I', 'am', 'a', 'test', 'tuple')))
